940c3fa75a64c6e16142c9c9be9ee96bc01df054
AryanKhubchandani <76617460+AryanKhubchandani@users.noreply.github.com>
2021-04-08 13:57:03 +0530

    Update version.kt

--- app/src/main/java/com/example/hscroll/version.kt
+++ app/src/main/java/com/example/hscroll/version.kt
@@ -7,16 +7,16 @@ class version(val url: String, val name: String) {
         fun getList(): List<version> {
             val versionList = ArrayList<version>()
             versionList.clear()
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/one.jpg", "Football"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/two.jpg", "Basketball"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/three.jpg", "Cricket"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/four.jpg", "Lawn Tennis"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/five.jpg", "Boxing"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/six.jpg", "Golf"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/seven.jpg", "Badminton"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/eight.jpg", "Table Tennis"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/nine.jpg", "Baseball"))
-            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/images/ten.jpg", "Hockey"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/one.jpg?raw=true", "Football"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/two.jpg?raw=true", "Basketball"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/three.jpg?raw=true", "Cricket"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/four.jpg?raw=true", "Lawn Tennis"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/five.jpg?raw=true", "Boxing"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/six.jpg?raw=true", "Golf"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/seven.jpg?raw=true", "Badminton"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/eight.jpg?raw=true", "Table Tennis"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/nine.jpg?raw=true", "Baseball"))
+            versionList.add(version("https://github.com/AryanKhubchandani/Hscroll/blob/master/images/ten.jpg?raw=true", "Hockey"))
             return versionList
         }
     }
